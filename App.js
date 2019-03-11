90bdf9dffbec39d26fa875bc73affa249af0336e
Aileen Rae <aileen@stormideas.com>
2019-03-11 18:33:59 +0000

    Basic placeholder container styles

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+
+const width = Dimensions.get('window').width;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.appTitle}>Daily</Text>
+
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder="Add an item!" />
+        </View>
       </View>
     );
   }
@@ -24,5 +30,18 @@ const styles = StyleSheet.create({
     marginTop: 60,
     marginBottom: 30,
     fontWeight: '300'
+  },
+  card: {
+    width: width - 20,
+    padding: 20,
+    marginBottom: 10,
+    backgroundColor: '#E4E4E4',
+    flex: 1
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24
   }
 });
